d714a43a18a98f69520c478c88968ad11d798461
angularacsi <walelignbalew2013@gmail.com>
2022-04-08 06:42:03 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int number;
+for (number = 48; number <= 57; number++)
+{
+if (number == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+
+return (0);
+}
+
