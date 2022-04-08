5ab083243c806fbfe3c05cb99c119b089147ced9
angularacsi <walelignbalew2013@gmail.com>
2022-04-08 05:35:44 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+char alphabet;
+
+for(alphabet=122;alphabet>=97;alphabet--)
+{
+putchar(alphabet);
+}
+
+puchar('\n');
+
+return(0);
+}
