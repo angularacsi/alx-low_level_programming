3c2bebaa619ad299c2dd62de6f1cf18b6bf3a20e
angularacsi <walelignbalew2013@gmail.com>
2022-04-12 15:08:28 +0300

    task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,7 +3,7 @@
 
 /**
  * print_times_table - Prints the times table of the input,
- *                     starting with 0.
+ * @n: the number to start counting from n to 98
  *  The value of the times table to be printed.
  */
 void print_times_table(int n)
